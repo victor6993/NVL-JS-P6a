7e403c1f32e10a43065e5bffd366588cafece821
Victor Wang Zhu <60756793+victor6993@users.noreply.github.com>
2020-03-15 15:18:37 +0100

    NVL-JS-P6a

---
+++ NVL-JS-P6a.js
@@ -0,0 +1,6 @@
+
+var nombreApellidos = "Víctor Wang Zhu";
+var edad = 26;
+var dias = ["lunes","martes","miércoles","jueves","viernes","sábado","domingo"];
+var gravedad = 9.8;
+var tresImpares = [1,3,5];
